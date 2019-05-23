b441aa7b55f704ff5e4ddedb693b3c447895a7d6
mickeykkim <mickeykkim@gmail.com>
2019-05-23 15:06:50 +0100

    did part 3

--- client/src/main.js
+++ client/src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
+import { sync } from 'vuex-router-sync'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+import store from '@/store/store'
 
 Vue.config.productionTip = false
 
@@ -13,10 +15,13 @@ Vue.use(Vuetify, {
   iconfont: 'md'
 })
 
+sync(store, router)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
